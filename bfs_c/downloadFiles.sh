bf043f93690619eebc5c54608e21dd1898996f3f
root <root@dhcp-15-194.it.uu.se>
2017-11-16 14:39:24 +0100

    bash script that downloads and unzips topcats

---
+++ bfs_c/downloadFiles.sh
@@ -0,0 +1,4 @@
+wget https://snap.stanford.edu/data/wiki-topcats.txt.gz
+wget  https://snap.stanford.edu/data/wiki-topcats-page-names.txt.gz
+gunzip wiki-topcats.txt.gz
+gunzip wiki-topcats-page-names.txt.gz
