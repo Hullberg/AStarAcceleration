a52ede914ca5d4c29a08cdb40a0409219a5d2e99
Hullberg <johan.hullberg@gmail.com>
2017-11-17 13:45:34 +0100

    Fix makefile, refactor C-code to underscores, remove unused code, added some comments

--- bfs_c/fileToMatrix.c
+++ bfs_c/fileToMatrix.c
@@ -1,50 +1,59 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int countlines(char* filename){
+int count_lines(char* filename){
   int ch = 'a';
-  int lines =0;
-  FILE *fp = fopen(filename,"r");
+  int lines = 0;
+  FILE *fp = fopen(filename, "r");
+
   while(!feof(fp)){
     ch = fgetc(fp);
-    if(ch == '\n')
-      {
-	printf("\n%d\n", lines);
-	lines++;
-      }
+    if(ch == '\n') {
+      //printf("\n%d\n", lines);
+      lines++;
+    }
   }
-  printf("%s  %d","UTE UR LOOP: ",lines);
+
+  printf("%s  %d \n","OUT OF LOOP: ", lines);
   fclose(fp);
+
   return lines;
 }
-int** createMatrix(int lines,char* filename){
+
+// The matrix represents edges between the vertices. 0 = no edge, 1 = edge.
+int** create_matrix(int lines, char* filename){
   int rows = lines;
   int columns = lines;
-  int **matrix = (int **)calloc(rows,sizeof(int*));
-  for(int i = 0; i < rows; i++){
-    matrix[i] = (int *)calloc(columns,sizeof(int));
-  } 
-  //int ch =0;
+
+  int **matrix = (int **)calloc(rows, sizeof(int*));
+  for(int i = 0; i < rows; i++) {
+    matrix[i] = (int *)calloc(columns, sizeof(int));
+  }
+
   int first_id =0;
   int second_id =0;
-  FILE *fp = fopen(filename,"r");
-  while(!feof(fp)){
-    fscanf(fp," %d %d",&first_id, &second_id);
+
+  FILE *fp = fopen(filename, "r");
+  while(!feof(fp)){ 
+    // Searches for two doubles, changes the first_id and second_id pointers to these points
+    // and updates the matrix at that row/column to indicate an edge.
+    fscanf(fp," %d %d", &first_id, &second_id);
     matrix[first_id][second_id]=1;
-    printf("%d %d", first_id,second_id);
+    //printf("%d %d", first_id, second_id); //Prints pairs of ID's with edge between.
   }
   fclose(fp);
+
   return matrix;
 }
 
-int** fileToMatrix(char* rowcountFile, char *IDfile){
-  int lines = countlines(rowcountFile);
-  int** matrix= createMatrix(lines,IDfile);
+int** file_to_matrix(char* row_count_file, char *id_file){
+  int lines = count_lines(row_count_file);
+  int** matrix= create_matrix(lines, id_file);
   return matrix;
 }
 
-int fileToRowCount(char* rowCountFile){
-  return countlines(rowCountFile);
+int file_to_row_count(char* row_count_file){
+  return count_lines(row_count_file);
 }
 
 /*
