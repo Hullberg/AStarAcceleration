81998bd2cb359d8b7f6231025669bf102a701e45
hullberg <johan.hullberg@gmail.com>
2017-11-22 02:42:52 -0800

    More comments/work on kernel+manager+cpucode

--- MadMaxeler/CPUCode/MemStreamCpuCode.c
+++ MadMaxeler/CPUCode/MemStreamCpuCode.c
@@ -8,39 +8,56 @@
 
 int main(void)
 {
+	// Multiple of 384 bytes due to array size (64) burst-aligned
 	const int size = 384;
 	int sizeBytes = size * sizeof(int32_t);
-	int32_t *x = malloc(sizeBytes);
+	int32_t *x = malloc(sizeBytes); //TODO Change this to Vertex struct, should be ALL vertices.
 	int32_t *y = malloc(sizeBytes);
 	int32_t *s = malloc(sizeBytes);
-	int scalar = 3; 
+	int32_t *u = malloc(sizeBytes);
+	int scalar = 5; // TODO This should be goal ID.
 
 
 	for(int i = 0; i<size; ++i) {
 		x[i] = random() % 100;
-		y[i] = random() % 100;
+		y[i] = random() % 100 +1;
+		u[i] = 0;
 	}
 
-
-	// Unable to find where these functions are defined
 	// The manager gets them in EngineCode/src/memstream/MemStreamManager.maxj
-	// I assume this is where we write the vertices & edges to
+	// Write all vertices to LMem.
 	printf("Writing to LMem.\n");
 	MemStream_writeLMem(0, sizeBytes, x);
 
 
-	// s is the output variable
-	// I assume this is where we add unvisited vertices.
+	// First run we send in the ID of the start-vertix. Result is an int* of children's ID's.
+	// Send those ID's through the same process.
+	// Repeat until goal is found.
 	printf("Running on DFE.\n");	
-	MemStream(scalar, size, y, s);
+	MemStream(scalar, size, y, u, s);
 	
 
-	for(int i=0; i<size; ++i)
+	for(int i=0; i<size; ++i){
 		// The line below is in kernel:  DFEVar sum = x + y + a;
+		// This will be executed for that index when the data is ready
 		// Followed by io.output("s", sum, type)
-		if (s[i] != x[i] + y[i] + scalar)
-			return 1;
+		printf("s=%d, x= %d, y= %d, sc= %d, u = %d  \n", s[i], x[i+1], y[i], scalar, u[i]);
+		//if (s[i] != x[i] + y[i] + scalar + u[i])
+			//return 1;
 
+	}
+	//printf("First done, next one up! \n");
+	/*for(int i = 0; i<size; ++i) {
+		y[i] = s[i];
+	}
+	MemStream(scalar, size, y, s);
+	for(int i=0; i<size; ++i){
+			// The line below is in kernel:  DFEVar sum = x + y + a;
+			// This will be executed for that index when the data is ready
+			// Followed by io.output("s", sum, type)
+			printf("%d = %d + %d  \n", s[i], x[i], y[i]);
+
+		}*/
 	printf("Done.\n");
 	return 0;
 }
