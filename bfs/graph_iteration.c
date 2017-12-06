c46da97cbd72483cf02371091613439c03bcc7d5
root <root@dhcp-15-194.it.uu.se>
2017-12-06 11:32:02 +0100

    c code done

--- bfs/graph_iteration.c
+++ bfs/graph_iteration.c
@@ -10,47 +10,37 @@
 #include "vertex.c"
 
 #define EDGE_COUNT 5
-#define VERTEX_COUNT 1000
+#define VERTEX_COUNT 1000000
 
 Vertex** vertex_list;
 
-int32_t* edges[EDGE_COUNT];
-/*
-int32_t* edge_0;
-int32_t* edge_1;
-int32_t* edge_2;
-int32_t* edge_3;
-int32_t* edge_4;
-*/
+int32_t* edges[EDGE_COUNT]; // [][][][][]
+                            // [][][][][]
+                            // [][][][][]
+                            // [][][][][]
+                            // x VERTEX_COUNT
 ////////////
 /////// INIT
 ////////////
 
-Vertex* create_vertex(){
- 
-  
+Vertex* create_vertex(){  
   Vertex* vertex = malloc(sizeof(Vertex));
-  vertex->value = rand() % 100001; // between 1 and 100000
+  vertex->value = rand() % 1000001; // between 1 and 100000
   vertex->edges = malloc(sizeof(int) * EDGE_COUNT);
   for (int i = 0; i < EDGE_COUNT; i++) {
     vertex->edges[i] = rand() % VERTEX_COUNT;
-    printf("i: %d  index: %d\n", i, vertex->edges[i]);
+    //printf("i: %d  index: %d\n", i, vertex->edges[i]);
   }
   return vertex;
 }
 
 void init_vertices(){
- 
   for (int i = 0; i < VERTEX_COUNT; i++) {
     vertex_list[i] = create_vertex();
   }
 }
 
-void init_arrays(){
-
-  for (int i = 0; i < EDGE_COUNT; i++){
-    edges[i] = malloc(sizeof(int) * VERTEX_COUNT);
-  }
+void update_edges() {
   for (int i = 0; i < VERTEX_COUNT; i++) {
     for (int j = 0; j < EDGE_COUNT; j++) {
       edges[j][i] = vertex_list[vertex_list[i]->edges[j]]->value;
@@ -58,28 +48,58 @@ void init_arrays(){
   }
 }
 
+void init_arrays(){
+  for (int i = 0; i < EDGE_COUNT; i++){
+    edges[i] = malloc(sizeof(int) * VERTEX_COUNT);
+  }
+  update_edges();
+}
+
 ////////////////
 /////// FUNCTION
 ////////////////
 
-void magic_function(){
-
+void magic_function(int index){
+  int32_t sum = 0;  
+  for (int i = 0; i < EDGE_COUNT; i++) {
+    sum += edges[i][index];
+  }
+  vertex_list[index]->value = sum / EDGE_COUNT;
 }
 
-void iterator(){
-
+void iterate(){
+  for (int i = 0; i < VERTEX_COUNT; i++) {
+    //start.stream(blah,blah,blah);
+    magic_function(i);
+  }
+  update_edges();
 }
 
-void replace_read_array(){
-
-}
-
-void check_pass_fail(){
-
+bool is_equal(int32_t a, int32_t b){
+  if (a == b) return true;
+  else return false;
 }
 
 bool converged(){
-  return false;
+  int index_one, index_two, value_one, value_two;
+  bool is_eq;
+  int counter = 0;
+  int max = 1000;
+  for (int i = 0; i < max; i++) {
+    index_one = rand() % (VERTEX_COUNT);
+    index_two = rand() % (VERTEX_COUNT);
+    value_one = vertex_list[index_one]->value;
+    value_two = vertex_list[index_two]->value;
+    
+    is_eq = is_equal(value_one, value_two);
+    if (is_eq) {
+      //printf("PASS ---- %d == %d at indices %d and %d\n", value_one, value_two, index_one, index_two);
+      counter++;
+    } 
+    //else printf("FAIL ---- %d != %d indices %d and %d\n", value_one, value_one, index_one, index_two);
+  }
+  if (counter == max) return true;
+  else return false;
 }
 
 ////////////
@@ -93,11 +113,21 @@ int main(int argc, char* argv[]) {
   init_vertices();
 
   init_arrays();
+  
+  int count = 0;
+  do {
+    iterate();
+    count++;
+  } while(!converged());
+  printf("\nDone! Converged after %d iterations.\n", count);
 
-  for (int i = 0; i < VERTEX_COUNT; i++) {
-    printf("i: %d    edges0: %d    edges1: %d    edges2: %d    edges3: %d    edges4: %d\n",
-	   i, edges[0][i], edges[1][i], edges[2][i], edges[3][i], edges[4][i]);
+  for (int i = 0; i < VERTEX_COUNT; i++){
+    free(vertex_list[i]->edges);
+    free(vertex_list[i]);
   }
-
+  for (int i = 0; i < EDGE_COUNT; i++) {
+    free(edges[i]);
+  }
+  free(vertex_list);
   return 0;
 }
